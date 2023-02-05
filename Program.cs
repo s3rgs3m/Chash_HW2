22a89eb4dc670245ca38878a7b418c17d827323b
s3rgs3m <sergsem@gmail.com>
2023-02-05 12:37:40 +0300

    Задача 15

--- Program.cs
+++ Program.cs
@@ -22,24 +22,30 @@ Console.Write ("Введите номер задачи 10, 13 или 15: ");
 if (int.TryParse(Console.ReadLine(), out int taskNum)){
     switch (taskNum) {
         case 10: //Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-                Console.Clear();
-                Console.WriteLine("Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.");
-                Console.Write("\nВведите число: ");
-                if (int.TryParse(Console.ReadLine(), out int number10))
-                    if (number10 < 100 || number10 > 999)
-                        Console.WriteLine ("Число должно быть трехзначным");
-                    else 
-                        Console.WriteLine ($"Вторая цифра числа {number10}: {number10 % 100 / 10}");
-                else
-                    Console.WriteLine ("Число введено не правильно");                
+            Console.Clear();
+            Console.WriteLine("Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.");
+            Console.Write("\nВведите число: ");
+            if (int.TryParse(Console.ReadLine(), out int number10))
+                if (number10 < 100 || number10 > 999)
+                    Console.WriteLine ("Число должно быть трехзначным");
+                else 
+                    Console.WriteLine ($"Вторая цифра числа {number10}: {number10 % 100 / 10}");
+            else
+                Console.WriteLine ("Число введено неправильно");                
             break;
 
         case 13: // Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
             break;
 
-        case 15:
-
+        case 15: // Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+            Console.Clear();
+            Console.WriteLine("Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.");
+                Console.Write("\nВведите номер дня недели: ");
+                if (int.TryParse(Console.ReadLine(), out int number15) && (number15 <8) && (number15 >0))
+                        Console.WriteLine (number15 > 5 ? "выходной" : "рабочий");
+                else
+                    Console.WriteLine ("Номер дня недели введен неправильно");       
             break;
         default:
             Console.WriteLine("Такого номера задачи нет");
